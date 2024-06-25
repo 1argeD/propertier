5c165c1d3901bb53971fe239c77a42f1cf154517
1argeD <94172354+1argeD@users.noreply.github.com>
2024-06-25 13:29:09 +0900

    <chore>스프링 클라우드 설정 기능 사용 - 코드 수정

--- src/main/java/com/example/properties/ProjectNameRestController.java
+++ src/main/java/com/example/properties/ProjectNameRestController.java
@@ -1,14 +1,14 @@
 package com.example.properties;
 
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@PropertySource("some.properties")
 @RefreshScope//새로고침 - 정보 변경 반영
 public class ProjectNameRestController {
     private final String projectName;
